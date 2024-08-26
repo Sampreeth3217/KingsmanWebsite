5d0cd2aca54243177324233e6a7f750f71abac14
Sampreeth3217 <nschowdary1234@gmail.com>
2024-08-26 23:39:31 +0530

    Co-authored-by: Pavan Kumar Chennupati <pavankc005@gmail.com> Co-authored-by: Tummuru Vimala Reddy <TVimala@users.noreply.github.com>

---
+++ Frontend/src/Components/login/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+function Login() {
+  return (
+    <div>
+        <h1>Login</h1>
+        <div className='row'>
+        <div className='col-11 col-sm-10 col-md-6 mx-auto'>
+            <form action="" className='p-'></form>
+         </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ Frontend/src/Components/region/Region.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+function Region() {
+  return (
+    <div>Region</div>
+  )
+}
+
+export default Region
