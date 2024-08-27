35ae6ecb0333b1614648ee8e5112761263c5b869
TVimala <vimalareddytummuru@gmail.com>
2024-08-27 14:37:10 +0530

    add:files

--- Frontend/src/Components/region/Region.jsx
+++ Frontend/src/Components/region/Region.jsx
@@ -10,19 +10,19 @@ function Region() {
         <div className='col-11 col-sm-10 col-md-6 mx-auto'>
             <form action="" className='p-5 login rounded'>
             <div className='mb-3'>
-         <label htmlFor="region" className='form-label'>Region</label>
-         <input type="text" id='region'{...register('region',{required:true})} className='form-control' />
-          {errors.region?.type==='required' && <p className='text-danger'>Region required</p>}
+         <label htmlFor="state" className='form-label'>State</label>
+         <input type="text" id='state'{...register('state',{required:true})} className='form-control' />
+          {errors.state?.type==='required' && <p className='text-danger'>State required</p>}
+        </div>
+        <div className='mb-3'>
+         <label htmlFor="district" className='form-label'>District</label>
+         <input type="text" id='district'{...register('district',{required:true})} className='form-control' />
+          {errors.district?.type==='required' && <p className='text-danger'>Districtname required</p>}
         </div>
         <div className='mb-3'>
          <label htmlFor="village" className='form-label'>Village</label>
          <input type="text" id='village'{...register('village',{required:true})} className='form-control' />
           {errors.village?.type==='required' && <p className='text-danger'>Villagename required</p>}
-        </div>
-        <div className='mb-3'>
-         <label htmlFor="region" className='form-label'>State</label>
-         <input type="text" id='Region'{...register('region',{required:true})} className='form-control' />
-          {errors.username?.type==='required' && <p className='text-danger'>statename required</p>}
         </div>
         <div className='d-flex justify-content-evenly'>
             <button className='btn btn-primary'>Select Region</button>
